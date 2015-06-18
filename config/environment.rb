fb986bf401a524d106dc20dd9ebe5ba0e78bb1f6
khanhbq <khanhbq@zigexn.vn>
2015-06-18 16:33:14 +0700

    change app name, add gems for develop

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'lotus/setup'
 require_relative '../lib/lotus_shop'
-require_relative '../apps/web/application'
+require_relative '../apps/shop/application'
 
 Lotus::Container.configure do
   mount Web::Application, at: '/'
